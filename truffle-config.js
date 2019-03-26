f187c3cb92c304a39b831ce93708ff9fa50efc48
Veronica Zheng <zhengzhixing@gmail.com>
2019-03-26 16:47:22 -0700

    configure networks rinkeby

--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,10 @@
  *
  */
 
-// const HDWalletProvider = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require('truffle-hdwallet-provider');
+// need to set environment variables INFURA_KEY and MNEMONIC
+const infuraKey = process.env.INFURA_KEY;
+const mnemonic = process.env.MNEMONIC;
 
 module.exports = {
   /**
@@ -46,6 +45,17 @@ module.exports = {
      host: "127.0.0.1",     // Localhost (default: none)
      port: 8545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
+    },
+    rinkeby: {
+      provider: function() {
+        if (!mnemonic || !infuraKey) {
+          throw new Error("Please set environment variables INFURA_KEY and MNEMONIC.")
+        }
+       return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/" + infuraKey);
+      },
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000,
     },
 
     // Another network with more advanced options...
